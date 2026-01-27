b9b513af28b575571bfe714585e787b783f1a344
adam <adamdweber@gmail.com>
2026-01-26 20:43:26 -0500

    more ticket2

--- exercises/05_ticket_v2/07_unwrap/src/lib.rs
+++ exercises/05_ticket_v2/07_unwrap/src/lib.rs
@@ -2,7 +2,11 @@
 //   When the description is invalid, instead, it should use a default description:
 //   "Description not provided".
 fn easy_ticket(title: String, description: String, status: Status) -> Ticket {
-    todo!()
+    let mut description = description;
+    if description.is_empty() || description.len() > 500 {
+        description = "Description not provided".to_string();
+    }
+    Ticket::new(title, description, status).unwrap()
 }
 
 #[derive(Debug, PartialEq, Clone)]
@@ -22,17 +26,12 @@ enum Status {
 impl Ticket {
     pub fn new(title: String, description: String, status: Status) -> Result<Ticket, String> {
         if title.is_empty() {
-            return Err("Title cannot be empty".to_string());
+            panic!("Title cannot be empty");
         }
         if title.len() > 50 {
-            return Err("Title cannot be longer than 50 bytes".to_string());
+            panic!("Title cannot be longer than 50 bytes");
         }
-        if description.is_empty() {
-            return Err("Description cannot be empty".to_string());
-        }
-        if description.len() > 500 {
-            return Err("Description cannot be longer than 500 bytes".to_string());
-        }
+
 
         Ok(Ticket {
             title,
