8d044fba382fed9803771132d568102efc31ac27
adam <adamdweber@gmail.com>
2026-02-16 22:53:17 -0500

    futures

--- exercises/08_futures/02_spawn/src/lib.rs
+++ exercises/08_futures/02_spawn/src/lib.rs
@@ -3,8 +3,27 @@ use tokio::net::TcpListener;
 // TODO: write an echo server that accepts TCP connections on two listeners, concurrently.
 //  Multiple connections (on the same listeners) should be processed concurrently.
 //  The received data should be echoed back to the client.
+
+async fn echo(listener: TcpListener) -> Result<(), anyhow::Error> {
+    loop {
+        let (mut stream, _addr) = listener.accept().await?;
+        tokio::spawn( async move {
+            let (mut reader, mut writer) = stream.split();
+            tokio::io::copy(&mut reader, &mut writer).await.unwrap();
+        });
+    }
+}
 pub async fn echoes(first: TcpListener, second: TcpListener) -> Result<(), anyhow::Error> {
-    todo!()
+    let first_handle = tokio::spawn(echo(first));
+    let second_handle = tokio::spawn(echo(second));
+
+    let (first_result, second_result) = tokio::join!(first_handle, second_handle);
+    //let second_result = tokio::join!(second_handle);
+    first_result??;
+    second_result??;
+
+    Ok(())
+
 }
 
 #[cfg(test)]
--- exercises/08_futures/03_runtime/src/lib.rs
+++ exercises/08_futures/03_runtime/src/lib.rs
@@ -2,6 +2,7 @@
 //  accept connections on both of them concurrently, and always reply to clients by sending
 //  the `Display` representation of the `reply` argument as a response.
 use std::fmt::Display;
+use std::sync::Arc;
 use tokio::io::AsyncWriteExt;
 use tokio::net::TcpListener;
 
@@ -10,7 +11,21 @@ where
     // `T` cannot be cloned. How do you share it between the two server tasks?
     T: Display + Send + Sync + 'static,
 {
-    todo!()
+    let reply = Arc::new(reply);
+    let first_handle = tokio::spawn(replier(first, Arc::clone(&reply)));
+    let second_handle = tokio::spawn(replier(second, reply));
+    tokio::join!(first_handle, second_handle);
+}
+
+async fn replier<T>(listener: TcpListener, reply: Arc<T>)
+where
+    T: Display + Send + Sync + 'static
+{
+    loop {
+        let (mut stream, _addr) = listener.accept().await.unwrap();
+        let (mut _reader, mut writer) = stream.split();
+        writer.write_all(format!("{}", reply).as_bytes()).await.unwrap();
+    }
 }
 
 #[cfg(test)]
